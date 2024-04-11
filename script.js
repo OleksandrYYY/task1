8aa2cda89c0d84782c3547245d37286577820a0f
Oleksandr Horbenko <oleksandr.horbenko.01@gmail.com>
2024-04-12 01:16:48 +0300

    Create variables

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let numberOne = 10;
+let numberTwo = 5;
+
+let sum = numberOne + numberTwo;
+console.log(sum);
+
+let subtraction = numberOne - numberTwo;
+console.log(subtraction);
+
+let multiply = numberOne * numberTwo;
+console.log(multiply);
+
+let division = numberOne / numberTwo;
+console.log(division);
+
+let exponentiation = numberTwo ** 3;
+console.log(exponentiation);
