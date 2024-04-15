1b110664f18671fa49d721d7df55bc192bb2b4da
Oleksandr Horbenko <oleksandr.horbenko.01@gmail.com>
2024-04-15 23:19:21 +0300

    Change of name variables

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 "use strict";
 
-let numberOne = 10;
-let numberTwo = 5;
+const numberOne = 10;
+const numberTwo = 5;
 
-let sum = numberOne + numberTwo;
+const sum = numberOne + numberTwo;
 console.log(sum);
 
-let subtraction = numberOne - numberTwo;
+const subtraction = numberOne - numberTwo;
 console.log(subtraction);
 
-let multiply = numberOne * numberTwo;
+const multiply = numberOne * numberTwo;
 console.log(multiply);
 
-let division = numberOne / numberTwo;
+const division = numberOne / numberTwo;
 console.log(division);
 
-let exponentiation = numberTwo ** 3;
+const exponentiation = numberTwo ** 3;
 console.log(exponentiation);
